a386ffed6cec784ce9b750d32b1c15d278045ca7
Jose Pehovaz-Diez <jose.pehovazdiez@gmail.com>
2019-01-30 20:53:00 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,4 +1,16 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
+  whole_name = ""
+  data.each do |season_number, attribute|
+    attribute.each do |value|
+      if season == season_number
+        whole_name = value["name"]
+        puts whole_name
+        binding.pry
+      end
+    end 
+  end
   # code here
 end
 
